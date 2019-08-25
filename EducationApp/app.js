6fe4da0423d5dbf5815725e10ca5616a31a72dc5
Medved2001 <misha.medvedev2001@gmail.com>
2019-08-25 15:35:31 +0300

    Level 1 commit

--- EducationApp/app.js
+++ EducationApp/app.js
@@ -1,1 +1,13 @@
-console.log(1);
+var money,type;
+money = prompt("What's your budget for this month?");
+type = prompt("Enter your date of birth YYYY-MM-DD");
+
+var ex1 = prompt("Enter what do you have to buy each day"), ex2=prompt("Hom much will it cost?");
+
+
+var addData = 
+{
+   budget:money, timeData:type,expenses:{ex1:ex2},optionalExpenses:0,savings:false
+};
+
+alert(money/30);
